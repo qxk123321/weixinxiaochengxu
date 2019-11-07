c3b75358086793333cc957a3cb23e602eaca1dd5
qxk123321 <1060344367@qq.com>
2019-11-07 19:34:18 +0800

    second commit

---
+++ miniprogram-8/pages/index/index.js
@@ -0,0 +1,42 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    isPlayingMusic:false
+  },
+  bgm:null,
+  music_url:'http://localhost/03.mp3',
+  music_coverImgUrl:'../images/timg3.jpg',
+  onReady:function(){
+    this.bgm=wx.getBackgroundAudioManager()
+    this.bgm.title='marry me'
+    this.bgm.epname='wedding'
+    this.bgm.singer='singer'
+    this.bgm.coverImgUrl=this.music_coverImgUrl
+    this.bgm.onCanplay(()=>{
+      this.bgm.pause()
+    })
+    this.bgm.src=this.music_url
+  },
+  play:function(e){
+    if(this.data.isPlayingMusic){
+      this.bgm.pause()
+    }
+    else{this.bgm.play()}
+    this.setData({
+      isPlayingMusic: !this.data.isPlayingMusic
+  })
+  },
+  callGroom:function(e){
+    wx.makePhoneCall({
+      phoneNumber: '13700000000',
+    })
+  },
+  callBride:function(e){
+    wx.makePhoneCall({
+      phoneNumber: '15600000000',
+    })
+  }
+})
---
+++ miniprogram-8/pages/video/video.js
@@ -0,0 +1,20 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    movieList:[{
+     create_time:'15325119734589',title:'海边随拍',
+     src:''
+    },
+    {
+      create_time: '15325119734589', title: '勿忘心安',
+      src: ''
+    },
+    {
+      create_time: '15325119734589', title: '点滴记忆',
+      src: ''
+    }]
+  }
+})
